88c5fbda5a6f6bf86caed69f6764c8891f9626f6
White Ace <whiteace@sina.com>
2017-08-15 19:19:54 +0800

    and design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import {Button, NavBar, Icon} from 'antd-mobile';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar
+          leftContent="back"
+          mode="dark"
+          onLeftClick={() => console.log('onLeftClick')}
+          rightContent={[< Icon key = "0" type = "search" style = {{ marginRight: '0.32rem' }}/>, <Icon key="1" type="ellipsis" / >]}>NavBar</NavBar>
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo"/> */}
           <h2>Welcome to React</h2>
         </div>
+        <Button size="small" className="btn" loading>loading button</Button>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
+        <Button type="primary">Button</Button>
       </div>
     );
   }
